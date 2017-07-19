a3f6dfd950aa3fa2d714dbfab9d5924bc0a277d8
hkinsman <howardkinsman@gmail.com>
2017-07-19 16:33:01 +0200

    Resonance!

--- workspace/DoubleBars/2017results/1-1-102-1/DoubleBars_v2.f90
+++ workspace/DoubleBars/2017results/1-1-102-1/DoubleBars_v2.f90
@@ -24,7 +24,7 @@
 
      step = 1.e-7 !vary step size
      start=0.
-     end=35 !vary end time
+     end=25 !vary end time
 
       m1=.5   !blue
       m2=.5  !red
@@ -41,12 +41,12 @@
      ys(7) = 0         ! m2,y
      ys(8) = 1	!m2,vy
 
-     ys(9) = -.1021    ! m3,x
+     ys(9) = -.102    ! m3,x
      ys(10) = 0       ! m3,vx
      ys(11) = 0        ! m3,y
      ys(12) = -.1		!m3, vy
 
-     ys(13) = .1021     ! m4,x
+     ys(13) = .102     ! m4,x
      ys(14) = 0      ! m4,vx
      ys(15) = 0        ! m4,y
      ys(16) = .1		!m4, vy
